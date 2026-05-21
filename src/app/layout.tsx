0baaa64d84049e08839becf3efdf7798170ad511
Rafael Kikuchi <rafaelk262002@gmail.com>
2026-05-21 18:17:30 -0300

    update for google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export const metadata: Metadata = {
   },
   title: "ADAC Church | Simplismente Célula",
   description: "ADAC Church é uma igreja cristã localizada em Caraguatatuba, São Paulo, Brasil. Fundada em 2006, a ADAC tem como missão compartilhar o amor de Deus e promover a transformação de vidas por meio do evangelho. A igreja oferece uma variedade de ministérios e atividades para atender às necessidades espirituais, emocionais e sociais de seus membros e da comunidade em geral. Com uma abordagem acolhedora e inclusiva, a ADAC busca ser um lugar onde as pessoas possam encontrar esperança, propósito e crescimento espiritual.",
+
+  verification: {
+    google: "google77c20e930726bf20",
+  },
 };
 
 export default function RootLayout({
